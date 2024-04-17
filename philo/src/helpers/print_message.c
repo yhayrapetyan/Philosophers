9ce6f985e56ab025d72d6c38a36731ed9c402377
Yura <211003yura@gmail.com>
2024-04-17 15:52:15 +0400

    orginized the files, there is a lock-order-inversion error what need to fix

--- philo/src/helpers/print_message.c
+++ philo/src/helpers/print_message.c
@@ -9,9 +9,6 @@ void		print_message(t_data *data, int id, char *message)
 	can_prt = can_print(data);
 	pthread_mutex_lock(&data->mut_print);
 	if (can_prt == 1 && get_philo_state(&data->philos[id - 1]) != DEAD)//potential issue
-		write(1, message, ft_strlen(message));
-	write(1, "\n", 1);
-	(void)time;
-//		printf("%lu %d %s\n", time, id, message);
+		printf("%lu %d %s\n", time, id, message);
 	pthread_mutex_unlock(&data->mut_print);
 }
