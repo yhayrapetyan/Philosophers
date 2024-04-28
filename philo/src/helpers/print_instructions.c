10637ad24c218b37c2bae536a5feea0a5a5d7557
Yura <211003yura@gmail.com>
2024-04-28 19:54:21 +0400

    update print instruction function, add some colors

--- philo/src/helpers/print_instructions.c
+++ philo/src/helpers/print_instructions.c
@@ -14,18 +14,20 @@
 
 int	print_instructions(void)
 {
+	printf( RED);
 	printf("\t\tWRONG INPUT!\n\n");
-	printf("./philo nb_philos time_to_die ");
-	printf("time_to_eat time_to_sleep ");
-	printf("number_of_times_each_philosopher_must_eat ");
-	printf("(optional argument)\n");
+	printf( YELLOW);
+	printf("./philo nb_philos die_t ");
+	printf("eat_t sleep_t ");
+	printf("[nb_meals]\n\n");
 	printf("Example:\n");
 	printf("\t./philo 4 800 200 200 5\n\n");
+	printf( GREEN);
 	printf("nb_philos: 1-200\n");
 	printf("time_to_die: 60+\n");
 	printf("time_to_eat: 60+\n");
 	printf("time_to_sleep: 60+\n");
-	printf("number_of_times_each_philosopher_must_eat: ");
-	printf("0+\n");
+	printf("nb_of_times_each_philo_must_eat: 0+\n");
+	printf( RESET_COLOR);
 	return (1);
 }
